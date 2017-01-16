10d3c3807b73b991225677f9a1d2928b6d0384d2
mellzamora <mellzamora@outlook.com>
2017-01-16 17:48:59 +0800

    updated assets

--- src/app.php
+++ src/app.php
@@ -11,6 +11,23 @@ $app->register(new ServiceControllerServiceProvider());
 $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
+
+$app['app_vars'] = ['page_title' => 'Local Development',
+                    'app_name'   => 'verbose-umbrella',
+                    'projroot'   => './',
+                    'dir'        => ["/Users/rommel.zamora/Documents/DevFiles/default/htdocs/sites/*"],
+                    'tld'        => 'dev',
+                    'icons'      => ['apple-touch-icon.png', 'favicon.ico'],
+                    'devtools'   => [['name' => 'Github', 'url' => 'https://github.com/', 'logo'=>'img/team/tools/tools-logo_01.jpg', 'desc'=>'Code Repository'],
+                                     ['name' => 'Bitbucket', 'url' => 'https://bitbucket.org/', 'logo'=>'img/team/tools/tools-logo_02.jpg', 'desc'=>'Code Repository'],
+                                     ['name' => 'Packagist', 'url' => 'https://packagist.org/', 'logo'=>'img/team/tools/tools-logo_03.jpg', 'desc'=>'Package Manager']],
+                    'siteoptions' => [
+                        //  'dirname' => 'Display Name',
+                        //	'dirname' => ['displayname' => 'DisplayName', 'adminurl' => 'http://something/admin' ],
+                        ],
+                    'hiddensites' => ['home']];
+
+
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
 
