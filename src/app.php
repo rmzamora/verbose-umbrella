695002abb7250e8d6f0bcb977a1c28053b8e079e
mellzamora <mellzamora@outlook.com>
2017-01-18 13:22:34 +0800

    updated code directory traverse.

--- src/app.php
+++ src/app.php
@@ -14,10 +14,18 @@ $app->register(new HttpFragmentServiceProvider());
 
 $app['app_vars'] = ['page_title' => 'Local Development',
                     'app_name'   => 'verbose-umbrella',
+                    'header_title' => 'mell zamora\'s MAC',
                     'projroot'   => './',
-                    'dir'        => ["/Users/rommel.zamora/Documents/DevFiles/default/htdocs/sites/*"],
+                    // change to your local directory
+                    'dir'        => ["/Users/rommel.zamora/Documents/DevFiles/default/htdocs/sites"],
                     'tld'        => 'dev',
                     'icons'      => ['apple-touch-icon.png', 'favicon.ico'],
+                    'appimage'   => ['php'=>['img/portfolio/web/php-logo_01_preview.jpg',
+                                             'img/portfolio/web/php-logo_02_preview.jpg',
+                                             'img/portfolio/web/symfony2-logo_01_preview.jpg',
+                                             'img/portfolio/web/symfony2-logo_02_preview.jpg'],
+                                     'html'=>['img/portfolio/web/web-logo__01_preview.jpg',
+                                              'img/portfolio/web/web-logo__02_preview.jpg']],
                     'devtools'   => [['name' => 'Github', 'url' => 'https://github.com/', 'logo'=>'img/team/tools/tools-logo_01.jpg', 'desc'=>'Code Repository'],
                                      ['name' => 'Bitbucket', 'url' => 'https://bitbucket.org/', 'logo'=>'img/team/tools/tools-logo_02.jpg', 'desc'=>'Code Repository'],
                                      ['name' => 'Packagist', 'url' => 'https://packagist.org/', 'logo'=>'img/team/tools/tools-logo_03.jpg', 'desc'=>'Package Manager']],
@@ -30,7 +38,6 @@ $app['app_vars'] = ['page_title' => 'Local Development',
 
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
     // add custom globals, filters, tags, ...
-
     return $twig;
 });
 
--- src/controllers.php
+++ src/controllers.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -9,11 +10,50 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 Request::setTrustedProxies(array('127.0.0.1'));
 
 $app->get('/', function () use ($app) {
+
+
+    $hiddenSites = $app['app_vars']['hiddensites'];
+    $tld = $app['app_vars']['tld'];
+    $finder = new Finder();
+    $finder->files()->in($app['app_vars']['dir']);
+    $dirs = $finder->depth('== 0')->directories();
+
+    $apps = [];
+
+    foreach ($dirs as $dir) {
+
+        if ( in_array( $dir->getBasename(), $hiddenSites ) ) continue;
+        $apps[$dir->getBasename()]['name'] = $dir->getBasename();
+
+        $dirSplit = explode('/', $dir->getPathname());
+        $dirName = $dirSplit[count($dirSplit)-2];
+        $apps[$dir->getBasename()]['dirname'] = $dirName;
+
+        $url = sprintf( 'http://%1$s.%2$s.%3$s', $dir->getBasename(), $dirName, $tld );
+
+        $apps[$dir->getBasename()]['url'] = $url;
+
+        $files = $finder->files()->name('composer.json')->in($dir->getPathname());
+        if(count($files) > 0) {
+            $apps[$dir->getBasename()]['type'] = 'php';
+            $apps[$dir->getBasename()]['img'] = $app['app_vars']['appimage']['php'][array_rand($app['app_vars']['appimage']['php'])];
+        } else {
+            $apps[$dir->getBasename()]['type'] = 'html';
+            $apps[$dir->getBasename()]['img'] = $app['app_vars']['appimage']['html'][array_rand($app['app_vars']['appimage']['html'])];
+        }
+    }
+
     return $app['twig']->render('index.html.twig',
                                 ['page_title'=> $app['app_vars']['page_title'],
                                  'app_name'  => $app['app_vars']['app_name'],
-                                 'devtools'  => $app['app_vars']['devtools']]);
+                                 'header_title' => $app['app_vars']['header_title'],
+                                 'devtools'  => $app['app_vars']['devtools'],
+                                 'apps'      => $apps]);
 })->bind('homepage');
+
+$app->get('/info', function () {
+    return phpinfo();
+})->bind('info');
 
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
     if ($app['debug']) {
